652204b5aef0487680409fe9fedc62ec156cfd29
bogidemasrepo <bogidemas@gmail.com>
2025-12-22 05:45:43 +0300

    add friends detail page

---
+++ app/root/friends/_layout.tsx
@@ -0,0 +1,13 @@
+import { Stack } from 'expo-router'
+import React from 'react'
+
+const ExploreLayout = () => {
+  return (
+    <Stack screenOptions={{headerShown:false}}>
+      <Stack.Screen name='index' />
+      <Stack.Screen name='[slug]' />
+    </Stack>
+  )
+}
+
+export default ExploreLayout
--- app/root/friends/index.tsx
+++ app/root/friends/index.tsx
@@ -1,10 +1,10 @@
+import Header from '@/components/header';
+import { API_URL } from '@/constants';
 import { useAuth } from '@/context/auth-context';
-import { db } from '@/firebase.config';
-import { Ionicons } from '@expo/vector-icons';
-import { Link } from 'expo-router';
-import { collection, getDocs } from 'firebase/firestore';
+import { Image } from 'expo-image';
+import { router } from 'expo-router';
 import React, { useEffect, useState } from 'react';
-import { FlatList, Text, View } from 'react-native';
+import { FlatList, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 const Explore = () => {
   const {user}=useAuth();
@@ -12,49 +12,108 @@ const Explore = () => {
         id: string;
         name: string;
         email: string;
+        avator:string;
     }[]>([]);
-  async function exploreUsers() {
-      const usersCol = collection(db, 'users');
-      const userSnapshot = await getDocs(usersCol);
-      const userList = userSnapshot.docs.map(doc =>{
-        const {id,name,email}= doc.data();return {id,name,email} as {id:string,name:string,email:string}
-      });
-      setUsers(userList);
-  }
-  useEffect(()=>{
-    exploreUsers();
-  },[])
-  return (<SafeAreaView style={{flex:1}}>
-    <View style={{width:"98%",borderRadius:4,marginHorizontal:"1%",height:56,display:"flex",flexDirection:"row",alignItems:"center",justifyContent:"space-between",borderWidth:1,borderColor:"#e9e7e7ff"}}>
-          <View style={{display:"flex",flexDirection:"row",alignItems:"flex-end"}}>
-          <View style={{width:48,height:48,display:"flex",borderRadius:4,borderWidth:1,borderColor:"#ccccccff"}}></View>
-              <Text style={{fontSize:14,margin:8,fontWeight:"bold",textAlign:"center",color:"#b4b4b4ff"}}>{user?.name||"Unknoun User"}</Text>
-          </View>
-          <View>
-            <Ionicons name="notifications-outline" size={24} color="#858585ff" />
-          </View>
-        </View>
-   
-        <FlatList data={users} contentContainerStyle={{padding:"1%",gap:4}} renderItem={({item})=>(
-          <Link  href={{
-            pathname: "/detail/[slug]",
+  
+    const RenderChatItem = ({ item }:{ item: {
+        id: string;
+        name: string;
+        email: string;
+        avator:string;
+    } }) => {
+      const handleNaviate=()=>{
+        router.navigate({
+            pathname: "/root/friends/[slug]",
             params: { slug: [user?.id.trim(), item.id].sort().join('_') }
-          }}>
-          <View style={{width:"100%",borderRadius:4,display:"flex",borderWidth:1,borderColor:"#d3d3d3ff",padding:4,flexDirection:"row",justifyContent:"space-between"}}>
-           <View style={{display:"flex",flexDirection:"row"}}>
-            <View style={{height:48,width:48,borderRadius:'100%',borderWidth:1,borderColor:"#e9e7e7ff"}}></View>
-            <View style={{height:48,display:"flex",justifyContent:"space-between",padding:4,alignItems:"flex-start"}}>
-              <Text style={{fontSize:10,color:"#b4b4b4ff",textAlign:"center"}}>{item.name.length>12?item.name.slice(0,12)+"...":item.name}</Text>
-               <Text style={{fontSize:10,color:"#b4b4b4ff",textAlign:"center"}}> Last message!</Text>
+          }
+        )
+      }
+      return (<>
+    <TouchableOpacity style={styles.chatCard} onPress={handleNaviate}>
+          <View style={styles.avatarContainer}>
+            <Image source={{ uri: item.avator }} style={styles.avatar} />
+          </View>
+          
+          <View style={styles.chatInfo}>
+            <View style={styles.chatHeader}>
+              <Text style={styles.userName}>{item.name}</Text>
+              {/* <Text style={styles.chatTime}>{new Date(item.timestamp).toLocaleTimeString()}</Text> */}
             </View>
-            </View> 
-            <View style={{height:48,display:"flex",justifyContent:"space-between"}}>
+            <View style={styles.chatFooter}>
+               <Text style={styles.lastMessage} numberOfLines={1}>{item.email}</Text>
+              {/*
+              {2 > 0 && (
+                <View style={styles.unreadBadge}>
+                  <Text style={styles.unreadText}>{2}</Text>
+                </View>
+              )} */}
             </View>
           </View>
-          </Link>)} />
-          <View style={{height:80}}></View>
+        </TouchableOpacity>
+        </>)};
+
+  useEffect(()=>{
+    fetch(`${API_URL}/users/explore`,{
+      method:"GET",
+      headers:{
+        "Authorization":"Bearer "+user?.token
+      }
+    }).then(res=>{
+      return res.json()
+    }).then(data=>{
+      setUsers(data.data)
+    })
+  },[]) 
+  return (<SafeAreaView style={{flex:1,paddingHorizontal:4,backgroundColor:"#fff"}}>
+            <Header />
+              <View style={{marginTop:76}}></View>
+              <FlatList
+                data={users}
+                renderItem={({item})=>(
+                  <RenderChatItem item={item} />
+            )}/>
+        <View style={{height:80}}></View>
     </SafeAreaView>
   )
 }
+
+
+const styles = StyleSheet.create({
+  container: { flex: 1, backgroundColor: '#FFFFFF' },
+    chatCard: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 16,
+  },
+  avatarContainer: { position: 'relative' },
+  avatar: { width: 60, height: 60, borderRadius: 8 },
+  onlineDot: {
+    position: 'absolute',
+    bottom: 0,
+    right: 0,
+    width: 14,
+    height: 14,
+    borderRadius: 7,
+    backgroundColor: '#4ADE80',
+    borderWidth: 2,
+    borderColor: '#FFF',
+  },
+    chatInfo: { flex: 1, marginLeft: 15 },
+  chatHeader: { flexDirection: 'row', justifyContent: 'space-between', marginBottom: 4 },
+  userName: { fontSize: 16, fontWeight: '600', color: '#1A1A1A' },
+  chatTime: { fontSize: 12, color: '#A0A0A0' },
+  chatFooter: { flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' },
+  lastMessage: { fontSize: 14, color: '#666', flex: 1, marginRight: 10 },
+  unreadBadge: {
+    backgroundColor: '#7D01FF', // Your purple accent
+    minWidth: 20,
+    height: 20,
+    borderRadius: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingHorizontal: 6,
+  },
+  unreadText: { color: 'white', fontSize: 10, fontWeight: 'bold' }
+});
 
 export default Explore;
